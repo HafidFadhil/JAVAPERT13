8c2942ccaf489d0d1ee8eb119d8bc12c08e5f35b
HafidFadhil <hafidfadhil019@gmail.com>
2019-12-19 10:14:16 +0700

    PERTEMUAN 13-14

---
+++ 5-Organisasi Program dengan Package/src/Program_manual/hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program_manual;
+
+/**
+ * Nama  : Hafid Fadhil
+ * NIM   : A2.1900075
+ * Kelas : TI-1C
+ */
+public class hello {
+    public static void main(String[] args) {
+         System.out.println("hello world");
+    }
+}
